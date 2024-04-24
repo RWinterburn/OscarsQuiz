b90861ced7a8b8e57f9744381f72e9c2967e8a0f
Rob Winterburn <robertwinterburn@hotmail.co.uk>
2024-04-24 15:46:01 +0100

    added second set of question and answers only adding two for now to see if code works first

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ var questions = [
    ]
 
 },
-]
+{ question: "who won Best Actress?",
+answers : [
+  {text: "Jimmy Hendrix", correct : false},
+  {text: "Jimmy Tallon", correct : false},
+  {text: "Emma Stone", correct : true},
+  {text: "Jimmy Kimmel", correct : true},
+ ]
+
+}
+];
+
+var questionElement = document.getElementById("question")
     
     
 
