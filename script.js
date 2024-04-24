d04dc67f2339c9549ac95ffaf12dfc9c48e974a2
Rob Winterburn <robertwinterburn@hotmail.co.uk>
2024-04-24 15:41:24 +0100

    added first set of answers and question

---
+++ script.js
@@ -0,0 +1,15 @@
+var questions = [
+  { question: "who hosted the 2024 Oscars?",
+  answers : [
+    {text: "Jimmy Hendrix", correct : false},
+    {text: "Jimmy Tallon", correct : false},
+    {text: "Jimmy Fallon", correct : false},
+    {text: "Jimmy Kimmel", correct : true},
+   ]
+
+},
+]
+    
+    
+
+
