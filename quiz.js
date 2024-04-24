71fb9f0231d5fd638b80b951f6d00718a2b92cf0
Rob Winterburn <robertwinterburn@hotmail.co.uk>
2024-04-24 14:58:42 +0100

    added questions and answers to JavaScript

---
+++ quiz.js
@@ -0,0 +1,6 @@
+const quiz = {
+  topic1: {
+    questions: ["who hosted the 2024 Oscars?", "who won Best Actress?", "Who won best actor?", "Who won best Director?", "Who won best original score", "who won best costume design?", "who won best supporting actor?"],
+    answers: ["Jimmy Kimmel", "Emma Stone", "Cillian Murphy", "Christopher Nolan", "Ludwig Göransson", "Holly Waddington", "Robert Downey Jr"]
+  },
+}
