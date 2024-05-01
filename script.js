f8d2332d0a37414c3c823f04fc84554e805cfd64
Rob Winterburn <robertwinterburn@hotmail.co.uk>
2024-05-01 09:40:18 +0100

    modfied the javascript show question added current question and score

--- script.js
+++ script.js
@@ -7,30 +7,45 @@ const resultText = {
   wrong: "Wrong! Better Luck Next Time Scrub!",
 };
 
+const goNextMessage = " Click here for the next question.";
 
 
 const questions = [
   {
     question: "Who hosted the 2024 oscars?",
-    choices: ["Jimmy Kimmel", "Jimmy Fallon", "Jim Talent"],
-    correct: 0
+    choices: ["Jimmy Kimmel",
+     "Jimmy Fallon",
+      "Jim Talent",
+       "Tom Brady"],
+    correct: 0,
   },
   {
     question: "Who won best actor?",
-    choices: ["Killem All Merfield", "Tony Stark", "Cillian Murphy"],
-    correct: 2
+    choices: [
+      "Killem All Merfield",
+      "Tony Stark",
+      "Cillian Murphy",
+      "Stewie Griffin",
+    ],
+    correct: 2,
   },
   {
     question: "who won best actress?",
-    choices: ["Emma Stoned", "Emma Stone", "Emma Baked"],
-    correct: 1
+    choices: [
+      "Emma Stoned",
+     "Emma Stone",
+      "Emma Baked", 
+      "Emma Chonged"],
+    correct: 1,
   },
-]
+];
+
 let currentQuestion = 0;
+let score = 0;
 function showQuestion(){
   const questionText = document.getElementById("question")
-questionText.innerText=questions[currentQuestion].question;
-
+ const answers = document.querySelectorAll("label")
+console.log(questionText)
 const choices = document.getElementById("choices");
 choices.forEach((choice, index)=> {
   choice.innerText = questions[currentQuestion].currentQuestion[index];
